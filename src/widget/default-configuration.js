f7091d19c7f853abdaf7f480a4915ba03bb800c7
Alvaro Vasconcelos <ialvsconcelos@gmail.com>
2018-06-15 11:44:47 -0300

    Tradução dos termos em inglês, para o Português-BR

--- src/widget/default-configuration.js
+++ src/widget/default-configuration.js
@@ -1,15 +1,15 @@
 
 export const defaultConfiguration = {
-    titleClosed: 'Click to chat!',
-    titleOpen: 'Let\'s chat!',
+    titleClosed: 'Clique para conversar!',
+    titleOpen: 'Vamos conversar!',
     closedStyle: 'chat', // button or chat
     closedChatAvatarUrl: '', // only used if closedStyle is set to 'chat'
     cookieExpiration: 1, // in days. Once opened, closed chat title will be shown as button (when closedStyle is set to 'chat')
-    introMessage: 'Hello! How can we help you?',
-    autoResponse: 'Looking for the first available admin (It might take a minute)',
-    autoNoResponse: 'It seems that no one is available to answer right now. Please tell us how we can ' +
-    'contact you, and we will get back to you as soon as we can.',
-    placeholderText: 'Send a message...',
+    introMessage: 'Olá, no que podemos ajudar?',
+    autoResponse: 'Aguarde um momento enquanto conectamos você a um atendente...',
+    autoNoResponse: 'Eita, parece que não tem ninguém disponível no momento. Por favor nos diga como contactar' +
+    'você e assim que voltarmos, vamos lhe contactar.',
+    placeholderText: 'Envie uma mensagem...',
     displayMessageTime: true,
     mainColor: '#1f8ceb',
     alwaysUseFloatingButton: false,
